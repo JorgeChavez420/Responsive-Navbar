7552a39291a7044864cbda5fd1f988448cf2dbc4
Jorge Angel Chavez Guzman <kokito83012@hotmail.com>
2021-11-29 01:03:34 -0600

    Practica Finalizada

---
+++ Index.js
@@ -0,0 +1,8 @@
+const mobileMenuButton = document.getElementById("mobile_menu_button");
+const mobileMenu = document.getElementById("mobile_menu");
+
+mobileMenuButton.addEventListener('click', function(){
+    mobileMenu.classList.toggle('show');
+    mobileMenuButton.classList.toggle('clase')
+
+});    
